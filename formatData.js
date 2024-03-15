921ce24934cdd8d2466fb0bbfb29b1bc00f4b78d
Sergey Shappo <shappoff@gmail.com>
2024-03-15 11:20:34 +0300

    Add files via upload

---
+++ formatData.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const obj = JSON.parse(fs.readFileSync('./dist/res.json', 'utf8'));
+
+const formattedObj = {};
+
+obj.results.forEach(({result}) => {
+    const [tabType] = result.range.split('!');
+
+    formattedObj[tabType] = result.rawData.map((raw) => {
+        const [
+            title,
+            description,
+            coords,
+            village,
+            iconColor,
+            iconType
+        ] = raw;
+
+        return {
+            title: title.trim(),
+            description: description.trim(),
+            coords,
+            village,
+            iconColor,
+            iconType
+        };
+    });
+});
+
+fs.writeFileSync("./dist/res2.json", JSON.stringify(formattedObj));
+
+// console.log(formattedObj);
